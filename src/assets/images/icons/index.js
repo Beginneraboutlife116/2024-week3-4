4713ad554f098aeb2edeaa4ba1bf426fddc68e40
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-08-10 22:54:57 +0800

    Add new SVG and PNG for Locations page

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -1,8 +1,21 @@
 import CallSvg from "./call.svg";
+import CallDarkSvg from "./call--dark.svg";
 import MailSvg from "./mail.svg";
 import PaidSvg from "./paid.svg";
 import TimeSvg from "./time.svg";
+import TimeDarkSvg from "./time--dark.svg";
 import HeartSvg from "./heart.svg";
 import GlassesSvg from "./glasses.svg";
+import LocationSvg from "./location.svg";
 
-export { CallSvg, MailSvg, PaidSvg, TimeSvg, HeartSvg, GlassesSvg };
+export {
+  CallSvg,
+  CallDarkSvg,
+  MailSvg,
+  PaidSvg,
+  TimeSvg,
+  TimeDarkSvg,
+  HeartSvg,
+  GlassesSvg,
+  LocationSvg,
+};
---
+++ src/assets/images/stores/index.js
@@ -0,0 +1,7 @@
+import Store1Png from "./store-1.png";
+import Store2Png from "./store-2.png";
+import Store3Png from "./store-3.png";
+import Store4Png from "./store-4.png";
+import Store5Png from "./store-5.png";
+
+export { Store1Png, Store2Png, Store3Png, Store4Png, Store5Png };
