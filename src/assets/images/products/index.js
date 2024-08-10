bfb82c43fb2a73ca0ec18b97c0b2aa312b62527d
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-08-10 12:23:43 +0800

    Add optical and sunglasses images

--- src/assets/images/products/index.js
+++ src/assets/images/products/index.js
@@ -6,6 +6,22 @@ import ModelAlliancePngSM from "./model--alliance-sm.png";
 import ProductAlliancePng from "./product--alliance.png";
 import ProductAlliancePngSM from "./product--alliance-sm.png";
 
+// Optical
+import Optical1Png from "./optical-1.png";
+import Optical2Png from "./optical-2.png";
+import Optical3Png from "./optical-3.png";
+import Optical4Png from "./optical-4.png";
+import Optical5Png from "./optical-5.png";
+import Optical6Png from "./optical-6.png";
+
+// Sunglasses
+import Sunglasses1Png from "./sunglasses-1.png";
+import Sunglasses2Png from "./sunglasses-2.png";
+import Sunglasses3Png from "./sunglasses-3.png";
+import Sunglasses4Png from "./sunglasses-4.png";
+import Sunglasses5Png from "./sunglasses-5.png";
+import Sunglasses6Png from "./sunglasses-6.png";
+
 export {
   ModelBlackWhitePng1,
   ModelBlackWhitePng2,
@@ -14,4 +30,20 @@ export {
   ModelAlliancePngSM,
   ProductAlliancePng,
   ProductAlliancePngSM,
+
+  // Optical
+  Optical1Png,
+  Optical2Png,
+  Optical3Png,
+  Optical4Png,
+  Optical5Png,
+  Optical6Png,
+
+  // Sunglasses
+  Sunglasses1Png,
+  Sunglasses2Png,
+  Sunglasses3Png,
+  Sunglasses4Png,
+  Sunglasses5Png,
+  Sunglasses6Png,
 };
