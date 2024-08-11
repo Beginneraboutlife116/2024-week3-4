103cde9d9531eab6afb16061044a06f5ee79e14e
Beginneraboutlife116 <pythonforeye@gmail.com>
2024-08-11 23:10:22 +0800

    Remove base to test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/2024-week3-4/",
   resolve: {
     alias: {
       assets: "../../assets",
